601321e2cbb07d59f2185c5dfd2a93730cbdb334
孙璐媛 <1516569352@qq.com>
2022-07-18 21:11:35 +0800

    小孙上传

---
+++ test7_18（冒泡）.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int arr[10]={9,2,4,5,8,1,3,6,7,10};
+	//int i = 0;//趟数
+	int i = 0;
+	
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0;i < sz - 1;i++)//趟数
+	{
+		//每一趟
+		int j = 0;
+		for (j = 0; j < 10 - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
