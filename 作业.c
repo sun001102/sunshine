99037e47467bd01713397a89556b1cad5ed8edc6
孙璐媛 <1516569352@qq.com>
2022-07-18 20:37:06 +0800

    吴浩提交

---
+++ 作业.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	printf("a= % d b=%d \n", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a= % d b=%d ", a, b);
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//    int i = 0;
+//    for (i = 10000; i <= 99999; i++)
+//    {
+//        //ÅÐ¶ÏÊÇ·ñlily
+//        int j = 0;
+//        int sum = 0;
+//        for (j = 1; j <= 4; j++)
+//        {
+//            int k = (int)pow(10, j);
+//            sum += (i % k) * (i / k);
+//        }
+//        if (sum == i)
+//            printf("%d ", i);
+//    }
+//    return 0;
+//}
