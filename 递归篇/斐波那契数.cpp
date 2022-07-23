8cd36cb9811c115ee552dd968f6e6756849bca3b
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-07-23 23:27:25 +0800

    Add files via upload

---
+++ 递归篇/斐波那契数.cpp
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else if (n > 2)
+//	{
+//		return Fib(n - 1) + Fib(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//     ret=Fib(n);
+//	printf("%d", ret);
+//	return 0;
+//
+//}
