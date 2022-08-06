9ae2c5d2843fc9b88e2f94edc7eddf33e9dacf57
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-08-06 22:37:14 +0800

    Add files via upload

---
+++ 选择题.cpp
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//    char a[1000] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < 1000; i++)
+//    {
+//        a[i] = -1 - i;
+//    }
+//    printf("%d", strlen(a));
+//    return 0;
+//}
---
+++ 长度受限制.cpp
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char arr1[20] = "abcdef ";
+//	char arr2[] = "bit ";
+//	//strncpy(arr1, arr2, 10);
+//	/*strncat(arr1, arr2, 5);*/
+//	int ret=strncmp(arr1, arr2, 5);
+//	if (ret == 0)
+//		printf("==");
+//	else if (ret > 0)
+//		printf(">");
+//	else
+//		printf("<");
+//	//printf("%s ", arr1);
+//	return 0;
+//}
