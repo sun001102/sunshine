c54608c5e2a1496982d68bf967defe97cf304322
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-08-13 23:04:38 +0800

    Add files via upload

---
+++ strerror(错误信息).cpp
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	FILE* pf = fopen("test.test", "r");//只读
+//	if (pf == NULL)//打开成功 返回有效地址  
+//	{              //失败     NULL
+//		printf("%s\n", strerror(errno));//把错误码转换成错误信息
+//		return 1;
+//	}
+//	else
+//	{
+//		//
+//	}
+//	return 0;
+//}
---
+++ strtok(切割).cpp
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	const char* sep = "@.";
+//	char emile[] = "sunluyuan@xikedagaoxingxueyuan.com";
+//	char cp[40] = { 0 };
+//	strcpy(cp, emile);
+//	char* ret = NULL;
+//	for (ret = strtok(cp, sep);//`1´Î
+//		ret != NULL;//ÅÐ¶Ï
+//		ret = strtok(NULL, sep))//µ÷Õû
+//	{
+//		printf("%s\n", ret);
+//	}
+//	return 0;
+//}
